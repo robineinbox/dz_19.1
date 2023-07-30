c5112962c11bca6129fce273b32064399115a6b4
robineinbox <robin_e@inbox.ru>
2023-07-30 22:31:01 +0300

    changed

--- server.py
+++ server.py
@@ -33,39 +33,3 @@ class Server(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/html')
         self.end_headers()
         self.wfile.write(bytes(self.__get_html_from_git(), 'utf-8'))
-
-
-
-
-
-# import requests
-# import base64
-# from http.server import BaseHTTPRequestHandler
-#
-# HOSTNAME = 'localhost'
-# SERVER_PORT = 8000
-#
-#
-# class Server(BaseHTTPRequestHandler):
-#     '''    Класс сервера магазина    '''
-#
-#     def __get_html_from_git(self) -> str:
-#         '''
-#         Получает данные HTML-файла с удаленого репозитория
-#         :return: html-код
-#         '''
-#         headers = {
-#             'Authorization': 'token ' + 'YOUR_TOKEN'
-#         }
-#         data = requests.get(
-#             url=`https://api.github.com/repos/robineinbox/web-homework/contents/homework_1/html/index.html`
-#         )
-#         html_data = data.text
-#         return html_data
-#
-#     def do_GET(self):
-#         '''        Обрабатывает GET-запросы, отправленные на сервер        '''
-#         self.send_response(200)
-#         self.send_header('Content-type', 'text/html')
-#         self.end_headers()
-#         self.wfile.write(bytes(self.__get_html_from_git(), 'utf-8'))
